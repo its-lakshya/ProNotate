0057f255f85e85c728f9c581c3de87f744c2be24
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-09-16 21:29:08 +0530

    deleted the unnecessary data

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
     <div className={darkTheme ? 'dark': ''}>
       <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
         <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
-        <Footer/>
         <Routes/>
+        <Footer/>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import Search from "./Search";
 
 const Navbar = ({darkTheme, setDarkTheme}) => {
   return (
@@ -14,6 +15,7 @@ const Navbar = ({darkTheme, setDarkTheme}) => {
             {darkTheme ? 'Light' : 'Dark'}
         </button>
       </div>
+      <Search/>
     </div>
   );
 };
