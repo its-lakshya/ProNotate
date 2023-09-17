0927b203088b0c50c1e6d497881fc14d68634453
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-09-17 20:08:41 +0530

    cleaned the project folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Routes from "./components/Routes";
+import './App.css';
 
-const App = () => {
-  const [darkTheme, setDarkTheme] = useState(false);
-  console.log(darkTheme)
-
+function App() {
   return (
-
-    <div className={darkTheme ? 'dark': ''}>
-      <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
-        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
-        <Routes/>
-        <Footer/>
-      </div>
+    <div>
+      Hello
     </div>
   );
-};
+}
 
 export default App;
